eea058448befccbb04094606efa8a119e64b063e
Kyem Asa <turtlemoons@protonmail.com>
2022-12-29 22:56:54 -0600

    clean up working routes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const characters = require('marvel-api/lib/characters');
 const app = express();
 const PORT = 5000;
-const xmenCharacters = require('./data/xmen')
+const xmenData = require('./data/xmen')
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
 app.get("/api", (request, response) => {
-  response.json(xmenCharacters)
+  response.json(xmenData)
 });
 
 app.listen(PORT, () => {
