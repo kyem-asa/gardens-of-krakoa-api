cc89d3a81d9ff18dd0bea6b14fd08eddd0a2d2cf
Kyem Asa <turtlemoons@protonmail.com>
2022-12-31 10:49:57 -0600

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const characters = require('./data/Characters')
+const cors = require('cors')
+
+app.use(cors());
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
