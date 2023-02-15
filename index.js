04f536edfb9adc4f68dd54b1a4b9eed3b4ce2ece
kyem-asa <turtlemoons@protonmail.com>
2023-02-15 04:39:53 -0600

    main route working

--- index.js
+++ index.js
@@ -4,19 +4,36 @@ const PORT = process.env.PORT || 5000;
 const characters = require('./data/Characters')
 const cors = require('cors')
 const characterController = require('./controllers/characters')
+const connectDB = require('./config/database');
+const Character = require('./models/Characters');
+const router = express.Router()
 
 
-app.use(cors());
+
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
 
 characterController.saveCharacters();
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
+// Routes
+
+app.use(router);
+
+router.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/api/v1", (request, response) => {
-  response.json(characters)
-});
+app.get('/api/v1', (request, response) => {
+      Character.find()
+      .then(results => {
+          console.log(results)
+          response.json(results)
+      })
+      .catch(error => console.error(error))
+  })
+
 
 app.get("/api/:name", (request, response) => {
   const name = request.params.name.toLowerCase();
