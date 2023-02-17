241ff8179d3df1174e73ddf2a6902f2c6f840ee6
kyem-asa <turtlemoons@protonmail.com>
2023-02-17 15:00:05 -0600

    update db string

--- config/database.js
+++ config/database.js
@@ -19,7 +19,6 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING);
-    console.log(conn)
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
