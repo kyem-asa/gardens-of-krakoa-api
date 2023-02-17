f997a785b82f8100aad246b395c3a7b7b3aa2298
kyem-asa <turtlemoons@protonmail.com>
2023-02-17 15:04:28 -0600

    remove old connection function deploy working

--- config/database.js
+++ config/database.js
@@ -1,21 +1,5 @@
 const mongoose = require('mongoose')
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.DB_STRING, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     })
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`)
-//   } catch (err) {
-//     console.error(err)
-//     process.exit(1)
-//   }
-// }
-
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING);
