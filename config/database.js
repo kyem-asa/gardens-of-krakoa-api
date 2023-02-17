a6e1a7f3fea75a7c4cf82d88cc4833220a99df69
kyem-asa <turtlemoons@protonmail.com>
2023-02-17 10:18:39 -0600

    fix db

--- config/database.js
+++ config/database.js
@@ -18,17 +18,13 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
 }
+
 
 module.exports = connectDB
